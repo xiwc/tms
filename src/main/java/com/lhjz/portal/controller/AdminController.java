94c956d798f8e7b8a67ae43bd353656b0866dfa4
weicheng xi <weicheng.xi@newtouch.cn>
2016-05-19 20:12:02 +0800

    remove unused files and codes

--- src/main/java/com/lhjz/portal/controller/AdminController.java
+++ src/main/java/com/lhjz/portal/controller/AdminController.java
@@ -4,10 +4,8 @@
 package com.lhjz.portal.controller;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 import org.slf4j.Logger;
@@ -19,37 +17,21 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.lhjz.portal.base.BaseController;
-import com.lhjz.portal.entity.Config;
 import com.lhjz.portal.entity.Language;
 import com.lhjz.portal.entity.Project;
-import com.lhjz.portal.entity.Settings;
 import com.lhjz.portal.entity.Translate;
 import com.lhjz.portal.entity.security.Authority;
 import com.lhjz.portal.entity.security.User;
-import com.lhjz.portal.model.RespBody;
 import com.lhjz.portal.model.UserInfo;
-import com.lhjz.portal.pojo.ContactForm;
-import com.lhjz.portal.pojo.Enum.Key;
-import com.lhjz.portal.pojo.Enum.Module;
-import com.lhjz.portal.pojo.Enum.Page;
 import com.lhjz.portal.pojo.Enum.Status;
-import com.lhjz.portal.repository.ConfigRepository;
 import com.lhjz.portal.repository.FileRepository;
 import com.lhjz.portal.repository.ProjectRepository;
-import com.lhjz.portal.repository.SettingsRepository;
 import com.lhjz.portal.repository.TranslateRepository;
 import com.lhjz.portal.repository.UserRepository;
-import com.lhjz.portal.util.EnumUtil;
-import com.lhjz.portal.util.FileUtil;
-import com.lhjz.portal.util.JsonUtil;
-import com.lhjz.portal.util.MapUtil;
 import com.lhjz.portal.util.StringUtil;
 import com.lhjz.portal.util.WebUtil;
 
@@ -70,9 +52,6 @@ public class AdminController extends BaseController {
 	FileRepository fileRepository;
 
 	@Autowired
-	SettingsRepository settingsRepository;
-
-	@Autowired
 	ProjectRepository projectRepository;
 
 	@Autowired
@@ -80,9 +59,6 @@ public class AdminController extends BaseController {
 
 	@Autowired
 	UserRepository userRepository;
-
-	@Autowired
-	ConfigRepository configRepository;
 
 	@RequestMapping("login")
 	public String login(Model model) {
@@ -95,177 +71,7 @@ public class AdminController extends BaseController {
 	@RequestMapping()
 	public String home(Model model) {
 
-		List<Settings> settings = settingsRepository.findByPage(Page.Index);
-
-		List<Settings> bigImgs = new ArrayList<Settings>();
-		List<Settings> hotNews = new ArrayList<Settings>();
-		List<Settings> moreNews = new ArrayList<Settings>();
-
-		for (Settings settings2 : settings) {
-			if (settings2.getModule() == Module.BigImg) {
-				bigImgs.add(settings2);
-			} else if (settings2.getModule() == Module.HotNews) {
-				hotNews.add(settings2);
-			} else if (settings2.getModule() == Module.MoreNews) {
-				moreNews.add(settings2);
-			}
-		}
-
-		model.addAttribute("bigImgs", bigImgs);
-		model.addAttribute("hotNews", hotNews);
-		model.addAttribute("moreNews", moreNews);
-
 		return "admin/index";
-	}
-
-	@RequestMapping("about")
-	public String about(Model model) {
-
-		List<Settings> settings = settingsRepository.findByPage(Page.About);
-
-		List<Settings> bigImgs = new ArrayList<Settings>();
-		List<Settings> branches = new ArrayList<Settings>();
-		List<Settings> experts = new ArrayList<Settings>();
-
-		for (Settings settings2 : settings) {
-			if (settings2.getModule() == Module.BigImg) {
-				bigImgs.add(settings2);
-			} else if (settings2.getModule() == Module.Branch) {
-				branches.add(settings2);
-			} else if (settings2.getModule() == Module.Expert) {
-				experts.add(settings2);
-			}
-		}
-
-		model.addAttribute("bigImgs", bigImgs);
-		model.addAttribute("branches", branches);
-		model.addAttribute("experts", experts);
-
-		return "admin/about";
-	}
-
-	@RequestMapping("case")
-	public String case_(Model model) {
-
-		List<Settings> settings = settingsRepository.findByPage(Page.Case);
-
-		List<Settings> introductions = new ArrayList<Settings>();
-		List<Settings> mores = new ArrayList<Settings>();
-
-		for (Settings settings2 : settings) {
-			if (settings2.getModule() == Module.Introduction) {
-				introductions.add(settings2);
-			} else if (settings2.getModule() == Module.More) {
-				mores.add(settings2);
-			}
-		}
-
-		model.addAttribute("introductions", introductions);
-		model.addAttribute("mores", mores);
-
-		return "admin/case";
-	}
-
-	@RequestMapping("contact")
-	public String contact(Model model) {
-
-		Config config = configRepository.findFirstByKey(Key.Contact);
-
-		if (config != null) {
-			model.addAttribute("contact", JsonUtil.json2Object(config.getValue(), ContactForm.class));
-		} else {
-			model.addAttribute("contact", new ContactForm());
-		}
-
-		return "admin/contact";
-	}
-
-	@RequestMapping("env")
-	public String env(Model model) {
-
-		List<Settings> settings = settingsRepository.findByPage(Page.Env);
-
-		List<Settings> introductions = new ArrayList<Settings>();
-		List<Settings> mores = new ArrayList<Settings>();
-
-		for (Settings settings2 : settings) {
-			if (settings2.getModule() == Module.Introduction) {
-				introductions.add(settings2);
-			} else if (settings2.getModule() == Module.More) {
-				mores.add(settings2);
-			}
-		}
-
-		model.addAttribute("introductions", introductions);
-		model.addAttribute("mores", mores);
-
-		return "admin/env";
-	}
-
-	@RequestMapping("feature")
-	public String feature(Model model) {
-
-		List<Settings> settings = settingsRepository.findByPage(Page.Feature);
-
-		List<Settings> introductions = new ArrayList<Settings>();
-		List<Settings> mores = new ArrayList<Settings>();
-
-		for (Settings settings2 : settings) {
-			if (settings2.getModule() == Module.Introduction) {
-				introductions.add(settings2);
-			} else if (settings2.getModule() == Module.More) {
-				mores.add(settings2);
-			}
-		}
-
-		model.addAttribute("introductions", introductions);
-		model.addAttribute("mores", mores);
-
-		return "admin/feature";
-	}
-
-	@RequestMapping("health")
-	public String health(Model model) {
-
-		List<Settings> settings = settingsRepository.findByPage(Page.Health);
-
-		List<Settings> introductions = new ArrayList<Settings>();
-		List<Settings> mores = new ArrayList<Settings>();
-
-		for (Settings settings2 : settings) {
-			if (settings2.getModule() == Module.Introduction) {
-				introductions.add(settings2);
-			} else if (settings2.getModule() == Module.More) {
-				mores.add(settings2);
-			}
-		}
-
-		model.addAttribute("introductions", introductions);
-		model.addAttribute("mores", mores);
-
-		return "admin/health";
-	}
-
-	@RequestMapping("product")
-	public String product(Model model) {
-
-		List<Settings> settings = settingsRepository.findByPage(Page.Product);
-
-		List<Settings> introductions = new ArrayList<Settings>();
-		List<Settings> mores = new ArrayList<Settings>();
-
-		for (Settings settings2 : settings) {
-			if (settings2.getModule() == Module.Introduction) {
-				introductions.add(settings2);
-			} else if (settings2.getModule() == Module.More) {
-				mores.add(settings2);
-			}
-		}
-
-		model.addAttribute("introductions", introductions);
-		model.addAttribute("mores", mores);
-
-		return "admin/product";
 	}
 
 	@RequestMapping("user")
@@ -296,29 +102,6 @@ public class AdminController extends BaseController {
 		model.addAttribute("users", userInfos);
 
 		return "admin/user";
-	}
-
-	@RequestMapping("resource")
-	public String resource(Model model) {
-
-		String storePath = env.getProperty("lhjz.upload.img.store.path");
-		int sizeLarge = env.getProperty("lhjz.upload.img.scale.size.large", Integer.class);
-		int sizeHuge = env.getProperty("lhjz.upload.img.scale.size.huge", Integer.class);
-		int sizeOriginal = env.getProperty("lhjz.upload.img.scale.size.original", Integer.class);
-
-		// img relative path (eg:'upload/img/' & 640 & '/' )
-		model.addAttribute("path", FileUtil.joinPaths(storePath, sizeOriginal + "/"));
-		model.addAttribute("pathLarge", FileUtil.joinPaths(storePath, sizeLarge + "/"));
-		model.addAttribute("pathHuge", FileUtil.joinPaths(storePath, sizeHuge + "/"));
-		// list all files
-		model.addAttribute("imgs", fileRepository.findAll());
-
-		return "admin/resource";
-	}
-
-	@RequestMapping("article")
-	public String article(Model model) {
-		return "admin/article";
 	}
 
 	@RequestMapping("feedback")
@@ -405,59 +188,4 @@ public class AdminController extends BaseController {
 		return "admin/import";
 	}
 
-	@RequestMapping(value = "pageEnable", method = RequestMethod.POST)
-	@ResponseBody
-	public RespBody pageEnable(@RequestParam("page") String page, @RequestParam("enable") boolean enable) {
-
-		Page page2 = EnumUtil.page(page);
-
-		if (page2 == Page.Unknow) {
-			logger.error("设置页面不存在, Page: {}", page);
-			return RespBody.failed("设置页面不存在!");
-		}
-
-		Config config = configRepository.findFirstByKey(Key.PageEnable);
-
-		// page enable config exists.
-		if (config != null) {
-			String json = config.getValue();
-
-			@SuppressWarnings("unchecked")
-			Map<String, Object> map = JsonUtil.json2Object(json, Map.class);
-
-			if (map == null) {
-				map = new HashMap<>();
-			}
-
-			map.put(page, enable);
-
-			config.setValue(JsonUtil.toJson(map));
-
-			configRepository.saveAndFlush(config);
-		} else {// first set page enable config
-			Config config2 = new Config();
-
-			config2.setKey(Key.PageEnable);
-			config2.setValue(JsonUtil.toJson(MapUtil.objArr2Map(page, enable)));
-
-			config2.setUsername(WebUtil.getUsername());
-
-			configRepository.saveAndFlush(config2);
-		}
-
-		return RespBody.succeed();
-	}
-
-	@SuppressWarnings("unchecked")
-	@ModelAttribute("pageEnable")
-	public Map<String, Object> pageEnable() {
-
-		Config config = configRepository.findFirstByKey(Key.PageEnable);
-
-		if (config != null) {
-			return JsonUtil.json2Object(config.getValue(), Map.class);
-		}
-
-		return null;
-	}
 }
--- src/main/java/com/lhjz/portal/controller/RootController.java
+++ src/main/java/com/lhjz/portal/controller/RootController.java
@@ -3,10 +3,8 @@
  */
 package com.lhjz.portal.controller;
 
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 import javax.servlet.http.HttpServletRequest;
@@ -20,29 +18,19 @@ import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.lhjz.portal.base.BaseController;
 import com.lhjz.portal.component.MailSender2;
-import com.lhjz.portal.entity.Config;
 import com.lhjz.portal.entity.Feedback;
-import com.lhjz.portal.entity.Settings;
 import com.lhjz.portal.model.RespBody;
-import com.lhjz.portal.pojo.ContactForm;
 import com.lhjz.portal.pojo.Enum.Action;
-import com.lhjz.portal.pojo.Enum.Key;
-import com.lhjz.portal.pojo.Enum.Module;
-import com.lhjz.portal.pojo.Enum.Page;
 import com.lhjz.portal.pojo.Enum.Target;
 import com.lhjz.portal.pojo.FeedbackForm;
-import com.lhjz.portal.repository.ConfigRepository;
 import com.lhjz.portal.repository.FeedbackRepository;
-import com.lhjz.portal.repository.SettingsRepository;
 import com.lhjz.portal.util.DateUtil;
-import com.lhjz.portal.util.JsonUtil;
 import com.lhjz.portal.util.MapUtil;
 import com.lhjz.portal.util.StringUtil;
 import com.lhjz.portal.util.TemplateUtil;
@@ -62,12 +50,6 @@ public class RootController extends BaseController {
 	static final Logger logger = LoggerFactory.getLogger(RootController.class);
 
 	@Autowired
-	SettingsRepository settingsRepository;
-
-	@Autowired
-	ConfigRepository configRepository;
-
-	@Autowired
 	FeedbackRepository feedbackRepository;
 
 	@Autowired
@@ -82,44 +64,8 @@ public class RootController extends BaseController {
 	@Autowired
 	Environment env;
 
-	@SuppressWarnings("unchecked")
-	@ModelAttribute("pageEnable")
-	public Map<String, Object> pageEnable() {
-
-		Config config = configRepository.findFirstByKey(Key.PageEnable);
-
-		if (config != null) {
-			return JsonUtil.json2Object(config.getValue(), Map.class);
-		}
-
-		return null;
-	}
-
 	@RequestMapping()
 	public String home(HttpServletRequest request, Model model) {
-
-		List<Settings> settings = settingsRepository.findByPage(Page.Index);
-
-		List<Settings> bigImgs = new ArrayList<Settings>();
-		List<Settings> hotNews = new ArrayList<Settings>();
-		List<Settings> moreNews = new ArrayList<Settings>();
-
-		for (Settings settings2 : settings) {
-			if (settings2.getModule() == Module.BigImg) {
-				bigImgs.add(settings2);
-			} else if (settings2.getModule() == Module.HotNews) {
-				hotNews.add(settings2);
-			} else if (settings2.getModule() == Module.MoreNews) {
-				moreNews.add(settings2);
-			}
-		}
-
-		model.addAttribute("bigImgs", bigImgs);
-		model.addAttribute("hotNews", hotNews);
-		model.addAttribute("moreNews", moreNews);
-
-		logWithProperties(Action.Visit, Target.Page, Page.Index.name(),
-				request.getRemoteAddr());
 
 		return "landing/index";
 	}
@@ -127,159 +73,8 @@ public class RootController extends BaseController {
 	@RequestMapping("about")
 	public String about(Model model) {
 
-		List<Settings> settings = settingsRepository.findByPage(Page.About);
-
-		List<Settings> bigImgs = new ArrayList<Settings>();
-		List<Settings> branches = new ArrayList<Settings>();
-		List<Settings> experts = new ArrayList<Settings>();
-
-		for (Settings settings2 : settings) {
-			if (settings2.getModule() == Module.BigImg) {
-				bigImgs.add(settings2);
-			} else if (settings2.getModule() == Module.Branch) {
-				branches.add(settings2);
-			} else if (settings2.getModule() == Module.Expert) {
-				experts.add(settings2);
-			}
-		}
-
-		model.addAttribute("bigImgs", bigImgs);
-		model.addAttribute("branches", branches);
-		model.addAttribute("experts", experts);
-
 		return "landing/about";
 	}
-
-	@RequestMapping("case")
-	public String cases(Model model) {
-
-		List<Settings> settings = settingsRepository.findByPage(Page.Case);
-
-		List<Settings> introductions = new ArrayList<Settings>();
-		List<Settings> mores = new ArrayList<Settings>();
-
-		for (Settings settings2 : settings) {
-			if (settings2.getModule() == Module.Introduction) {
-				introductions.add(settings2);
-			} else if (settings2.getModule() == Module.More) {
-				mores.add(settings2);
-			}
-		}
-
-		model.addAttribute("introductions", introductions);
-		model.addAttribute("mores", mores);
-
-		return "landing/case";
-	}
-
-	@RequestMapping("diagnose")
-	public String diagnose(Model model) {
-		return "landing/diagnose";
-	}
-
-	@RequestMapping("contact")
-	public String contact(Model model) {
-
-		Config config = configRepository.findFirstByKey(Key.Contact);
-
-		if (config != null) {
-			model.addAttribute("contact",
-					JsonUtil.json2Object(config.getValue(), ContactForm.class));
-		} else {
-			model.addAttribute("contact", new ContactForm());
-		}
-
-		return "landing/contact";
-	}
-
-	@RequestMapping("env")
-	public String env(Model model) {
-
-		List<Settings> settings = settingsRepository.findByPage(Page.Env);
-
-		List<Settings> introductions = new ArrayList<Settings>();
-		List<Settings> mores = new ArrayList<Settings>();
-
-		for (Settings settings2 : settings) {
-			if (settings2.getModule() == Module.Introduction) {
-				introductions.add(settings2);
-			} else if (settings2.getModule() == Module.More) {
-				mores.add(settings2);
-			}
-		}
-
-		model.addAttribute("introductions", introductions);
-		model.addAttribute("mores", mores);
-
-		return "landing/env";
-	}
-
-	@RequestMapping("feature")
-	public String feature(Model model) {
-
-		List<Settings> settings = settingsRepository.findByPage(Page.Feature);
-
-		List<Settings> introductions = new ArrayList<Settings>();
-		List<Settings> mores = new ArrayList<Settings>();
-
-		for (Settings settings2 : settings) {
-			if (settings2.getModule() == Module.Introduction) {
-				introductions.add(settings2);
-			} else if (settings2.getModule() == Module.More) {
-				mores.add(settings2);
-			}
-		}
-
-		model.addAttribute("introductions", introductions);
-		model.addAttribute("mores", mores);
-
-		return "landing/feature";
-	}
-
-	@RequestMapping("health")
-	public String health(Model model) {
-
-		List<Settings> settings = settingsRepository.findByPage(Page.Health);
-
-		List<Settings> introductions = new ArrayList<Settings>();
-		List<Settings> mores = new ArrayList<Settings>();
-
-		for (Settings settings2 : settings) {
-			if (settings2.getModule() == Module.Introduction) {
-				introductions.add(settings2);
-			} else if (settings2.getModule() == Module.More) {
-				mores.add(settings2);
-			}
-		}
-
-		model.addAttribute("introductions", introductions);
-		model.addAttribute("mores", mores);
-
-		return "landing/health";
-	}
-
-	@RequestMapping("product")
-	public String product(Model model) {
-
-		List<Settings> settings = settingsRepository.findByPage(Page.Product);
-
-		List<Settings> introductions = new ArrayList<Settings>();
-		List<Settings> mores = new ArrayList<Settings>();
-
-		for (Settings settings2 : settings) {
-			if (settings2.getModule() == Module.Introduction) {
-				introductions.add(settings2);
-			} else if (settings2.getModule() == Module.More) {
-				mores.add(settings2);
-			}
-		}
-
-		model.addAttribute("introductions", introductions);
-		model.addAttribute("mores", mores);
-
-		return "landing/product";
-	}
-
 
 	@RequestMapping(value = "feedback/save", method = RequestMethod.POST)
 	@ResponseBody
